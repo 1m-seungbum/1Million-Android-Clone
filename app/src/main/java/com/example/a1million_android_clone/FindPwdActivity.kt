ee10dadd6698e88d7a3ae21498ebbeee2455f9d8
1m-seungbum <seungbum@1million.com>
2021-04-14 14:48:48 +0900

    add: 비밀번호 찾기 페이지 1. 비밀번호 찾기 페이지 화면 구성 완료

--- app/src/main/java/com/example/a1million_android_clone/FindPwdActivity.kt
+++ app/src/main/java/com/example/a1million_android_clone/FindPwdActivity.kt
@@ -2,10 +2,15 @@ package com.example.a1million_android_clone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_find_pwd.*
 
 class FindPwdActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_pwd)
+
+        find_pwd_button.setOnClickListener {
+            // 메일로 변경된 비밀번호 보내주기
+        }
     }
 }
